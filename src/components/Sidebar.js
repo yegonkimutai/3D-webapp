0ba2640930f4657af1f4455251298947a1f03616
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-02 13:59:51 +0300

    :sparkles:Add Navbar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,90 @@
+import { Link, NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Logo from '../assets/images/logo-s.png'
+import LogoSub from '../assets/images/logo_sub.png'
+import {
+    faHome,
+    faUser,
+    faEnvelope,
+    faSuitcase,
+    faBars,
+    faClose,
+  } from '@fortawesome/free-solid-svg-icons'
+  import {
+    faLinkedin,
+    faGithub,
+  } from '@fortawesome/free-brands-svg-icons'
+import './Sidebar.scss';
+
+const Sidebar = () => {
+    return(
+        <div className="nav-bar">
+            <Link to='/' className="logo">
+                <img src={Logo} alt="logo"/>
+                <img className="sub-logo" src={LogoSub} alt="logo"/>
+            </Link>
+            <nav>
+            <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        </nav>
+        <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/brian-yegon-kim05/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/yegonkimutai"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+        </div>
+    )
+}
+
+export default Sidebar

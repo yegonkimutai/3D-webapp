0f9863b9a03200f8ec8fcd5d5a92b3d3647ea4bd
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-06-03 12:57:15 +0300

    :sparkles:Add about section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Sidebar from './components/Sidebar';
+import About from './components/About';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout/>}/>
         <Route index element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
       </Routes>
     </div>
   );
